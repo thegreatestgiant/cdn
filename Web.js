dec852a0d421f6678f0f26ea515e783464f2ddf3
The Greatest Giant <96693544+thegreatestgiant@users.noreply.github.com>
2022-06-07 11:06:39 -0400

    Update and rename web.js to Web.js

--- Web.js
+++ Web.js
@@ -1,4 +1,4 @@
-var vaporwindow='new Nightmare({"title":"Whatsapp","type":"js","js":"https://all-b.cf/whatsapp.js","theme": "dark","width": 600,"height": 400})'
+var vaporwindow='new Nightmare({"title":"Whatsapp","type":"js","js":"https://cdn.jsdelivr.net/gh/thegreatestgiant/cdn@main/Whatsapp.js","theme": "dark","width": 600,"height": 400})'
 async function injectnightmare(){
   var e=await fetch("https://cdn.jsdelivr.net/gh/FogNetwork/Nightmare-Windows/windows.min.js"),
   n=await e.text()
