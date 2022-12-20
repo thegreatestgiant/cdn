3fbebbe9a10bf04d9b3edf73f571ef3067ab84ff
The Greatest Giant <seanalter1@gmail.com>
2022-12-20 17:03:03 -0500

    Update Adguard.sh

--- Adguard.sh
+++ Adguard.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
+# cool variable
+SPaCE="/\/\/\/\//\//\/\/\/\/\//\/\/\/\/\/\//\/\/\/\\/\/\/\/\\//\\/\//\\/\\/\/\/\\\/\\\/\/\\/\/\/\/\\\/\\/\"
+
 # Update
 #echo "Update first"
 #echo "yum -y update && yum upgrade -y"
 sudo yum -y update && sudo yum upgrade -y
 
 # Install epel-release
-echo "Install epel-release"
+echo "$SPaCE $SPaCE $SPaCE $SPaCE Install epel-release $SPaCE $SPaCE $SPaCE $SPaCE"
 EPEL="yum install epel-release -y"
-echo $EPEL
+echo $EPEL $SPaCE
 sudo $EPEL
 
 # get snapd
-echo "snapd stuff"
+echo "$SPaCE $SPaCE $SPaCE snapd stuff $SPaCE $SPaCE $SPaCE"
 sudo dnf info snapd
 sudo dnf -y install snapd
 
 # systemctl
-echo "Systemctl stuff"
+echo "$SPaCE $SPaCE $SPaCE Systemctl stuff $SPaCE $SPaCE $SPaCE"
 sudo systemctl status snapd.socket && systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
 #install adguard now
-echo "install adguard "
+echo "$SPaCE $SPaCE $SPaCE $SPaCE Install adguard $SPaCE $SPaCE $SPaCE"
 sudo snap info adguard-home
 sudo snap install adguard-home
 
 # Firewall stuff
-echo "Firewall Stuff"
+echo "$SPaCE $SPaCE $SPaCE $SPaCE Firewall Stuff $SPaCE $SPaCE $SPaCE $SPaCE"
 sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=53/udp --permanent
 sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --reload
 
 # make it work
-echo "make it work now"
+echo "$SPaCE $SPaCE $SPaCE make it work now $SPaCE $SPaCE $SPaCE"
 sudo systemctl stop systemd-resolved && systemctl disable systemd-resolved
 
 # You're done
