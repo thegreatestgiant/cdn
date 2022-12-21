e723078d5f59c027c84ebfdcc23307d530d7b4fa
The Greatest Giant <seanalter1@gmail.com>
2022-12-21 00:09:23 -0500

    Update adguard.sh

--- adguard.sh
+++ adguard.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
 
-# Update
-echo "Update first"
-sudo yum -y update && sudo yum upgrade -y
+# Variable
+bord="============================================================================================================================================================================"
 
 # Install epel-release
+echo $bord
 echo "Install epel-release"
+echo "It's ok id it says \"Failed loading plugin \"osmsplugin\": No module named 'librepo'\" epel-release will still download"
+echo $bord
 sudo yum install epel-release -y
 
 # get snapd
+echo $bord
 echo "snapd stuff"
-sudo dnf info snapd
-sudo dnf -y install snapd
+echo $bord
+sudo yum install snapd -y
 
 # systemctl
+echo $bord
 echo "Systemctl stuff"
-sudo systemctl status snapd.socket && sudo systemctl enable --now snapd.socket
+echo $bord
+sudo systemctl enable --now snapd.socket
+
+#transfer
 sudo ln -s /var/lib/snapd/snap /snap
 
+sleep 1
+
 #install adguard now
+echo $bord
 echo "Install adguard"
-sudo snap info adguard-home
+echo $bord
+sudo snap install snapd
+sleep 1
 sudo snap install adguard-home
 
 # Firewall stuff
+echo $bord
 echo "Firewall Stuff"
+echo $bord
 sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=53/udp --permanent
 sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --reload
 
 # make it work
+echo $bord
 echo "make it work now"
+echo $bord
 sudo systemctl stop systemd-resolved && sudo systemctl disable systemd-resolved
 
 # You're done
+echo $bord
 echo "log into your Adguard-home acct. now on port the port"
+echo $bord
