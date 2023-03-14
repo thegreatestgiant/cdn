1dabf64432edf624a5f1db5b452501cd05cbcc84
The Greatest Giant <seanalter1@gmail.com>
2023-03-14 15:09:12 -0400

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -i
 
+PACKAGES=(nano net-tools docker docker.io docker-compose nautilus bat nginx sshfs zip unzip cmatrix tree)
+
 setup_shell() {
     echo "alias bat='batcat'
     alias cmatrix='cmatrix -sb'
@@ -15,13 +17,62 @@ setup_shell() {
     exec bash
 }
 
-setup_packages() {
-    sudo apt update -y && sudo update-initramfs -u -y && sudo apt upgrade -y
+sudo apt update -y && sudo update-initramfs -u -y && sudo apt upgrade -y
 
-    sudo apt install -y nano net-tools docker docker.io docker-compose nautilus bat nginx sshfs zip unzip cmatrix tree
-
-    echo "Done!"
+install_all_packages() {
+  echo "Installing all packages without prompting..."
+  sudo apt install -y "${PACKAGES[@]}"
+  echo "Done!"
 }
 
-setup_packages
+install_selected_packages() {
+  echo "Please select which packages you don't want to install (enter the numbers separated by spaces):"
+  for i in "${!PACKAGES[@]}"; do
+    echo "[$i] ${PACKAGES[$i]}"
+  done
+
+  read -r -a choices
+  for choice in "${choices[@]}"; do
+    if [[ "$choice" =~ ^[0-9]+$ ]]; then
+      unset PACKAGES[$choice]
+    fi
+  done
+
+  echo "Installing selected packages..."
+  sudo apt install -y "${PACKAGES[@]}"
+  echo "Done!"
+}
+
+
+# Process command-line options
+while getopts ":hy" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 [-y]"
+      echo "  -y: automatically install all packages without prompting"
+      echo "  -h: display this help message"
+      exit 0
+      ;;
+    y)
+      install_all_packages
+      setup_shell
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo -n "Do you want to install all packages? [Y/n] "
+read choice_all
+choice_all="${choice_all:-y}"
+
+if [[ "$choice_all" =~ ^[Yy]$ ]]; then
+  install_all_packages
+else
+  install_selected_packages
+fi
+
 setup_shell
