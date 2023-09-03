ac489471e6da039b6a31cba7c55150fed3aafb52
The Greatest Giant <seanalter1@gmail.com>
2023-09-03 12:55:09 -0400

    Update Prozy.js

--- Prozy.js
+++ Prozy.js
@@ -1,5 +1,5 @@
 document.addEventListener("keyup",function (e){var key=e.key+e.location;if(key=="Shift2")if(main.style.visibility=="hidden"){main.style.visibility="visible";console.style.visibility="visible"}else{main.style.visibility="hidden";console.style.visibility="hidden"}})
 var iframe=document.createElement('iframe')
-iframe.setAttribute('src',"https://bd0d99eb-6531-46c9-9032-961a7d3d1ccb.id.repl.co")
+iframe.setAttribute('src',"https://d24495be-71d8-4db5-95db-cfddefe5ecc0.id.repl.co")
 iframe.setAttribute('style','scrollbar:none;position:relative;width:100%;height:100%;top:0px;right:0px;border:none;overflow:hidden')
 appid.appendChild(iframe)
