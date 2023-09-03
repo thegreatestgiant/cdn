7bd6b486c2fedb665c70921f95da35915be62530
The Greatest Giant <seanalter1@gmail.com>
2023-09-03 13:00:32 -0400

    Delete Whatsapp.js

--- Whatsapp.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("keyup",function (e){var key=e.key+e.location;if(key=="Shift2")if(main.style.visibility=="hidden"){main.style.visibility="visible";console.style.visibility="visible"}else{main.style.visibility="hidden";console.style.visibility="hidden"}})
-var iframe=document.createElement('iframe')
-iframe.setAttribute('src',"https://bd0d99eb-6531-46c9-9032-961a7d3d1ccb.id.repl.co/web/_aHR0cHM6Ly93ZWIud2hhdHNhcHAuY29t_/")
-iframe.setAttribute('style','scrollbar:none;position:relative;width:100%;height:100%;top:0px;right:0px;border:none;overflow:hidden')
-appid.appendChild(iframe)
